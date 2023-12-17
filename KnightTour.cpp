37414b5cccdc459220fbf135dd65d49e86b80f64
AlexDuna <daniel.alex17@yahoo.com>
2023-12-17 20:16:01 +0200

    Schimbare limbaj
    Am decis sa schimb limbajul din C in C++ pentru o implementare mai rapida din punct de vedere al scrisului de cod.

--- KnightTour.cpp
+++ KnightTour.cpp
@@ -13,3 +13,39 @@ din jocul de șah) astfel încât toate pozițiile tablei să fie acoperite.*/
     Voi implementa o functie de calculare a numarului de miscari posibile pentru cal la fiecare pozitie, iar apoi o functie recursiva
     pentru efectuarea miscarilor.
 */
+
+//Implementarea
+
+#include <iostream>
+using namespace std;
+
+//vectori pentru directiile/pozitiile pe care le poate lua calul pe randuri si coloane
+const int rows[8]={-1,1,2,2,1,-1,-2,-2};
+const int col[8]={2,2,1,-1,-2,-2,-1,1};
+
+//declaram global matricea de mutari, dimensiunea matricei si numarul mutarii
+int matrice_mutari[50][50],mutari,dimensiune_matrice;
+
+
+int main()
+{
+    cout << "Dimensiune matrice N x N:";
+    cin >> dimensiune_matrice;
+    int poz_rand , poz_coloana;
+    cout << "Introduceti pozitia de inceput a calului: ";
+    cin >> poz_rand>>poz_coloana;
+    matrice_mutari[poz_rand][poz_coloana] = 1;
+
+    //functia recursiva de traversare a miscarilor pe tabla de sah
+
+    for(int i = 1 ; i <= dimensiune_matrice ; i++)
+    {
+        for(int j = 1 ; j <= dimensiune_matrice ; j++)
+        {
+            cout << matrice_mutari[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
